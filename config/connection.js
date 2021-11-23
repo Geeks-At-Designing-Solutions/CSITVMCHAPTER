9c497eb059d6f7d2d9daa8405dbf09aa9039190b
Malavika <65840561+Malavika345@users.noreply.github.com>
2021-11-23 17:06:03 +0530

    database changed to atlasdb

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ const state={
 }
 
 module.exports.connect=function(done){
-    const url = 'mongodb://localhost:27017'
+    const url = 'mongodb+srv://CSI:xXf5pZpKyBnsVyU@cluster0.ebkpm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     const dbname = 'chapters'
-
+    
     mongoClient.connect(url,(err,data)=>{
         if(err) return done(err)
         state.db=data.db(dbname)
